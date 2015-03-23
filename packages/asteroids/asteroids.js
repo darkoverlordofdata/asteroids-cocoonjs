a77f6fa5e2ebfc3e9e2ee5e5ba5f147a6436eee2
darkoverlordofdata <darkoverlordofdata@gmail.com>
2015-03-23 11:53:51 -0700

    publish

--- packages/asteroids/asteroids.js
+++ packages/asteroids/asteroids.js
@@ -21,6 +21,7 @@
    */
   var Animation, AnimationNode, AnimationSystem, Asteroid, AsteroidCollisionNode, AsteroidDeathView, AsteroidView, Asteroids, Audio, AudioNode, AudioSystem, Bullet, BulletAgeNode, BulletAgeSystem, BulletCollisionNode, BulletView, Collision, CollisionSystem, DeathThroes, DeathThroesNode, DeathThroesSystem, Display, Dot, EntityCreator, ExplodeAsteroid, ExplodeShip, GameConfig, GameManager, GameNode, GameState, Gun, GunControlNode, GunControlSystem, GunControls, Hud, HudNode, HudSystem, HudView, KeyPoll, MersenneTwister, MotionControls, MovementNode, Physics, PhysicsControlNode, PhysicsControlSystem, PhysicsNode, PhysicsSystem, Point, Position, RenderNode, RenderSystem, ShootGun, Sound, Spaceship, SpaceshipDeathView, SpaceshipNode, SpaceshipView, SystemPriorities, WaitForStart, WaitForStartNode, WaitForStartSystem, WaitForStartView,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; },
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
@@ -311,7 +312,9 @@
       this.states = {};
       window.addEventListener('keydown', this.keyDownListener);
       window.addEventListener('keyup', this.keyUpListener);
-      this.gamePad(config);
+      if (__indexOf.call(window, 'ontouchstart') >= 0 || navigator.msMaxTouchPoints) {
+        this.gamePad(config);
+      }
     }
 
     KeyPoll.prototype.keyDownListener = function(event) {
